3cf88044f43dfcf3d3feb334385999a2f75d2b72
Ranga Rao Karanam <in28minutes@gmail.com>
2020-02-06 11:40:28 +0530

    IAAC

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -43,7 +43,7 @@ module "in28minutes-cluster" {
     {
       instance_type = "t2.micro"
       max_capacity  = 5
-      desired_capacity = 2
+      desired_capacity = 3
       min_capacity  = 2
     }
   ]
