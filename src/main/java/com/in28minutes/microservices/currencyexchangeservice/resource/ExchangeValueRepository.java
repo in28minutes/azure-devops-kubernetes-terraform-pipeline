4d40ab4805837ca573c8724c24aafe2ca14040b6
lessr <lessr@lightsource-ecom.com>
2020-12-30 21:00:42 -0500

    --->>>hello world - add comment to the ExchangeValueRepository file

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/ExchangeValueRepository.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/ExchangeValueRepository.java
@@ -1,8 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice.resource;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-//JPA repository interface  
+//JPA repository interface  new changes s
 public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
 	ExchangeValue findByFromAndTo(String from, String to);
 }
- 
+    
